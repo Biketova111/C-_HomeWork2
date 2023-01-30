4bf3180eaa818a45e0d452cf8a9865fe7384051c
Biketova111 <89241781930@mail.ru>
2023-01-30 21:27:20 +0900

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*  Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+using System.Security.Cryptography.X509Certificates;
+
+int GetNumber (string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int DayOfTheWeek = GetNumber("Введите номер дня недели: ");
+if (DayOfTheWeek >= 1 && DayOfTheWeek < 6)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+   if (DayOfTheWeek == 6 || DayOfTheWeek == 7)
+   {
+    Console.WriteLine("да");
+   }
+   else
+   {
+    Console.WriteLine("Нет такого дня недели");
+   }
+}
